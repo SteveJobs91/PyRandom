8834cb26f24156aa8945b25c8f0dbaeda7acd20e
SteveJobs91 <91110474+SteveJobs91@users.noreply.github.com>
2022-09-24 16:04:45 +0530

    PyRandom v1.0.1 has a new print statement!

--- pyrandom.py
+++ pyrandom.py
@@ -1,4 +1,5 @@
 print("Welcome to PyRandom! Have fun generating random numbers!")
+print("PyRandom is just a fun project I created to get familiar with Git & GitHub.")
 
 import random
 num = random.random()
